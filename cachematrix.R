75a3c6167b77d2ee817c60569e8b900afcc710c7
matostado <matostado@hotmail.com>
2016-01-24 06:56:00 -0600

    Cambios MATC
    Cambios MATC

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+##  Como en el ejemplo de MEAN solo cambiamos el nombre de variables
+##  y usamos la funcion inverse
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+     inversa <- NULL 
+     set <- function(y) { 
+         x <<- y 
+         inversa <<- NULL 
+     } 
+     get <- function() x 
+     setinversa <- function(inverse) inversa <<- inverse 
+     getinversa <- function() inversa 
+     list(set=set, get=get, setinversa=setinversa, getinversa=getinversa) 
 }
 
 
-## Write a short comment describing this function
+## 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+     inversa <- x$getinversa() 
+     if(!is.null(inversa)) { 
+         message("obteniendo datos ...") 
+         return(inversa) 
+     } 
+     data <- x$get() 
+     inversa <- solve(data) 
+     x$setinversa(inversa) 
+     inversa 
 }
+
+
